7095a90be6ee9b4752c0c40ebe76d274f693ab32
Fabian Mahdi <fabian@mahdi.at>
2023-12-27 21:44:05 +0100

    put db initializer into its own project

---
+++ DBInitializer/DbInitializer.cs
@@ -0,0 +1,52 @@
+using Npgsql;
+
+namespace SWEN.DbInitializer;
+
+public class DbInitializer(DbConfig config)
+{
+    private readonly DbConfig _config = config;
+
+    public void InitDB()
+    {
+        if(_config.DropDb)
+            DropDatabase();
+
+        CreateDatabase();
+    }
+
+    private void CreateDatabase()
+    {
+        try
+        {
+            NpgsqlConnection connection = new(_config.ConnectionString);
+            connection.Open();
+
+            string script = File.ReadAllText(_config.CreateScript); 
+
+            NpgsqlCommand command = new(script, connection);
+            command.ExecuteNonQuery();
+        }
+        catch(NpgsqlException ex)
+        {
+            Console.Error.WriteLine($"Failed to connect to database: { ex.Message }");
+        }
+    }
+
+    private void DropDatabase()
+    {
+        try
+        {
+            NpgsqlConnection connection = new(_config.ConnectionString);
+            connection.Open();
+
+            string script = File.ReadAllText(_config.DropScript); 
+
+            NpgsqlCommand command = new(script, connection);
+            command.ExecuteNonQuery();
+        }
+        catch(NpgsqlException ex)
+        {
+            Console.Error.WriteLine($"Failed to connect to database: { ex.Message }");
+        }
+    }
+}
