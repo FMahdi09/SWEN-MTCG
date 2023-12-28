3c0b4e611a4399aa22c1c7cc7b870431a33651f5
Fabian Mahdi <fabian@mahdi.at>
2023-12-28 22:12:22 +0100

    added GET /tradings

--- MTCG/Businesslogic/EndPointHandlers/TradingHandler.cs
+++ MTCG/Businesslogic/EndPointHandlers/TradingHandler.cs
@@ -70,4 +70,34 @@ public class TradingHandler(string connectionString)
            return new HttpResponse("500 Internal Server Error", JsonSerializer.Serialize(new Error("Unable to connect ot database")));
         }
     }
+
+    [EndPoint(HttpMethods.GET, "/tradings")]
+    public HttpResponse GetAllTradingDeals(HttpRequest request)
+    {
+        try
+        {
+            // create unit of work
+            using UnitOfWork unit = new(_connectionString, withTransaction: false);
+
+            // get user from token and check permission
+            if(unit.UserRepository.GetUser(request.GetToken()) is not User user)
+                return new HttpResponse("401 Unauthorized", JsonSerializer.Serialize("Access token is missing or invalid"));
+
+            // get trading deals
+            List<TradingDeal> tradingDeals = unit.TradingRepository.GetTradingDeals();
+
+            if(tradingDeals.Count == 0)
+                return new HttpResponse("204 No Content");
+
+            return new HttpResponse("200 OK", JsonSerializer.Serialize(tradingDeals));
+        }
+        catch(JsonException)
+        {
+            return new HttpResponse("400 Bad Request", JsonSerializer.Serialize(new Error("Invalid Body")));
+        }
+        catch(NpgsqlException)
+        {
+           return new HttpResponse("500 Internal Server Error", JsonSerializer.Serialize(new Error("Unable to connect ot database")));
+        }
+    }
 }
--- MTCG/DataAccess/Repositories/TradingRepository.cs
+++ MTCG/DataAccess/Repositories/TradingRepository.cs
@@ -39,4 +39,64 @@ public class TradingRepository(IDbConnection connection) : BaseRepository(connec
 
         return reader.Read();
     }
+
+    // READ
+    public List<TradingDeal> GetTradingDeals(User? user = null)
+    {
+        // create command
+        using NpgsqlCommand command = new();
+
+        if(user is not null)
+        {
+            command.CommandText = "SELECT td.guid as dealguid, td.userid as dealuser, td.mindamage, tdeal.name as dealcardtype, cc.guid as cardguid, c.name as cardname, c.damage as carddamage, c.id as cardid, e.name as elementname, tcard.name as cardcardtype " +
+                                  "FROM tradingdeals td " +
+                                  "JOIN createdcards cc ON td.cardid = cc.id " +
+                                  "JOIN cards c ON cc.cardid = c.id " +
+                                  "JOIN elements e ON c.elementid = e.id " +
+                                  "JOIN cardtypes tcard ON c.cardtypeid = tcard.id " +
+                                  "JOIN cardtypes tdeal ON td.cardtypeid = tdeal.id " +
+                                  "WHERE td.userid = @userid";
+
+            command.AddParameterWithValue("userid", DbType.Int32, user.Id);
+        }
+        else
+        {
+            command.CommandText = "SELECT td.guid as dealguid, td.userid as dealuser, td.mindamage, tdeal.name as dealcardtype, cc.guid as cardguid, c.name as cardname, c.damage as carddamage, c.id as cardid, e.name as elementname, tcard.name as cardcardtype " +
+                                  "FROM tradingdeals td " +
+                                  "JOIN createdcards cc ON td.cardid = cc.id " +
+                                  "JOIN cards c ON cc.cardid = c.id " +
+                                  "JOIN elements e ON c.elementid = e.id " +
+                                  "JOIN cardtypes tcard ON c.cardtypeid = tcard.id " +
+                                  "JOIN cardtypes tdeal ON td.cardtypeid = tdeal.id ";
+        }
+
+        // execute command
+        using IDataReader reader = ExecuteQuery(command);
+
+        List<TradingDeal> deals = [];
+
+        while(reader.Read())
+        {
+            Card card = new(
+                id: (int)reader["cardid"],
+                guid: (string)reader["cardguid"],
+                name: (string)reader["cardname"],
+                damage: (int)reader["carddamage"],
+                element: (string)reader["elementname"],
+                type: (string)reader["cardcardtype"]
+            );
+
+            TradingDeal deal = new(
+                tradeId: (string)reader["dealguid"],
+                userId: (int)reader["dealuser"],
+                card: card,
+                minDamage: (int)reader["mindamage"],
+                cardType: (string)reader["dealcardtype"]
+            );
+
+            deals.Add(deal);
+        }
+
+        return deals;
+    }
 }
